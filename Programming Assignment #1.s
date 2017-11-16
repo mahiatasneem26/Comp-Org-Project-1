71d8e46c3a3c6840d5a3578e412383e8f7c7c58e
mahiatasneem26 <33528212+mahiatasneem26@users.noreply.github.com>
2017-11-15 23:45:00 -0500

    Update Working code

--- Programming Assignment #1.s
+++ Programming Assignment #1.s
@@ -15,7 +15,7 @@ main:
 	syscall				#syscall to read user_input and store the string in memory
 
 	addi $t7, $t0, 8		#add the value of the 9th byte of user_input to the register $t7
-	addi $s5, $t0, 0
+	addi $s5, $t0, 0		#copy of $t0
 
 check_length:				#label to count length of user_input
 	lb $t1, 0($s5)
@@ -24,14 +24,18 @@ check_length:				#label to count length of user_input
 	addi $s3, $s3, 4
 	addi $s5, $s5, 1 
 	j check_length
+
 sub_four:				#label to branch to if value in first offset refers to new line or null
 	addi $s3, $s3, -4
+
+first_char:
+	lb $t1, 0($t0)
+	beq $t1, 10, if_invalid
 	
 iterate_string:				#iterate through user_input to check if all the charectars are valid	
 	
 	lb $t1, 0($t0)			#load first byte of memory into register $t1
-
-
+	#beq $t1, 32, space
 	beq $t1, 0, less_than_ten	#if the value at offset 0 < 8 or <10, branch to less_than 8
 	beq $t1, 10, less_than_ten
 	blt $t1, 48, if_invalid		#branch to if_invalid label if value in $t1 is less than 48 (ASCII dec for number 0)		
@@ -44,6 +48,11 @@ iterate_string:				#iterate through user_input to check if all the charectars ar
 	addi $s1, $0, 87
 	blt $t1, 103, if_valid	
 	bgt $t1, 102, if_invalid	#branch to if_invalid if value in $t1 is greater than 102 (ASCII dec for 'f')
+
+#space:
+#	addi $t0, $t0, 1
+#	addi $s3, $s3, -4
+#	bne $t0, $t7, iterate_string
 	
 if_invalid:				#label to call invalid_input and exit program
 	li $v0, 4			#call code to prompt invalid_input message
@@ -59,11 +68,11 @@ if_valid:				#label to call valid_input and exit program
 	add $s2, $s4, $s2		# add and store the value of $s4 and $s2 to $s2
 	bne $t0, $t7, iterate_string	#if offset of $t0 is not equal to $t7, branch to iterate_string to continue looping
 
-less_than_ten:			#if the value at offset 0 < 8 branch to this label
+less_than_ten:				#if the value at offset 0 < 8 branch to this label
 	addi $s0, $0, 10		#store 10 in $s0
 	addi $t0, $t0, -1		#subtract 1 from user_input
 	lb $t1, 0($t0)
-	blt $t1, 58, exit_loop		#if 0($t0) < 8, branch to exit_loop
+	blt $t1, 58, exit_loop		#if 0($t0) < 10, branch to exit_loop
 	divu $s2, $s0			# else divide by 10 and store in $s2
 	mflo $a0			# move lower bits to $a0 and print
 	li $v0, 1
